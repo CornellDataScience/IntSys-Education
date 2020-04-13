fe2fea9f7c4fa7b3f27c5601c5d56d504761bbad
Samar Khanna <samar.khanna@gmail.com>
2020-04-12 23:43:56 -0400

    Initial commit for a3, data + starter scripts for NNs and data loading

---
+++ a3/data_loader.py
@@ -0,0 +1,99 @@
+import csv
+import os
+
+import numpy as np
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler
+
+
+class SimpleDataset(Dataset):
+    """SimpleDataset [summary]
+    
+    [extended_summary]
+    
+    :param path_to_pkl: Path to PKL file with Images
+    :type path_to_pkl: str
+    :param path_to_labels: path to file with labels
+    :type path_to_labels: str
+    """
+    def __init__(self, path_to_pkl, path_to_labels):
+        ## TODO: Add code to read csv and load data. 
+        ## You should store the data in a field.
+        # Eg (on how to read .csv files):
+        # with open('path/to/.csv', 'r') as f:
+        #   lines = ...
+        ## Look up how to read .csv files using Python. This is common for datasets in projects.
+        pass
+
+    def __len__(self):
+        """__len__ [summary]
+        
+        [extended_summary]
+        """
+        ## TODO: Returns the length of the dataset.
+        pass
+
+    def __getitem__(self, index):
+        """__getitem__ [summary]
+        
+        [extended_summary]
+        
+        :param index: [description]
+        :type index: [type]
+        """
+        ## TODO: This returns only ONE sample from the dataset, for a given index.
+        ## The returned sample should be a tuple (x, y) where x is your input 
+        ## vector and y is your label
+        ## Before returning your sample, you should check if there is a transform
+        ## sepcified, and pply that transform to your sample
+        # Eg:
+        # if self.transform:
+        #   sample = self.transform(sample)
+        ## Remember to convert the x and y into torch tensors.
+
+        pass
+
+
+def get_data_loaders(path_to_pkl, 
+                     path_to_labels,
+                     train_val_test=[0.8, 0.2, 0.2], 
+                     batch_size=32):
+    """get_data_loaders [summary]
+    
+    [extended_summary]
+    
+    :param path_to_csv: [description]
+    :type path_to_csv: [type]
+    :param train_val_test: [description], defaults to [0.8, 0.2, 0.2]
+    :type train_val_test: list, optional
+    :param batch_size: [description], defaults to 32
+    :type batch_size: int, optional
+    :return: [description]
+    :rtype: [type]
+    """
+    # First we create the dataset given the path to the .csv file
+    dataset = SimpleDataset(path_to_csv, path_to_labels)
+
+    # Then, we create a list of indices for all samples in the dataset.
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+
+    ## TODO: Rewrite this section so that the indices for each dataset split
+    ## are formed. You can take your code from last time
+
+    ## BEGIN: YOUR CODE
+    train_indices = []
+    val_indices = []
+    test_indices = []
+    ## END: YOUR CODE
+
+    # Now, we define samplers for each of the train, val and test data
+    train_sampler = SubsetRandomSampler(train_indices)
+    train_loader = DataLoader(dataset, batch_size=batch_size, sampler=train_sampler)
+
+    val_sampler = SubsetRandomSampler(val_indices)
+    val_loader = DataLoader(dataset, batch_size=batch_size, sampler=val_sampler)
+
+    test_sampler = SubsetRandomSampler(test_indices)
+    test_loader = DataLoader(dataset, batch_size=batch_size, sampler=test_sampler)
+
+    return train_loader, val_loader, test_loader
---
+++ a3/neural_net.py
@@ -0,0 +1,69 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from data_loader import get_data_loaders
+from typing import List, Union, Tuple
+
+
+class SimpleNeuralNetModel(nn.Module):
+    """SimpleNeuralNetModel [summary]
+    
+    [extended_summary]
+    
+    :param layer_sizes: Sizes of the input, hidden, and output layers of the NN
+    :type layer_sizes: List[int]
+    """
+    def __init__(self, layer_sizes: List[int]):
+        super(SimpleNeuralNetModel, self).__init__()
+        # TODO: Set up Neural Network according the to layer sizes
+        # The first number represents the input size and the output would be
+        # the last number, with the numbers in between representing the
+        # hidden layer sizes
+        raise NotImplementedError()
+    
+    def forward(x):
+        """forward generates the prediction for the input x.
+        
+        :param x: Input array of size (Batch,Input_layer_size)
+        :type x: np.ndarray
+        :return: The prediction of the model
+        :rtype: np.ndarray
+        """
+        raise NotImplementedError()
+
+
+class SimpleConvNetModel(nn.Module):
+    """SimpleConvNetModel [summary]
+    
+    [extended_summary]
+    
+    :param img_shape: size of input image as (W, H)
+    :type img_shape: Tuple[int, int]
+    :param output_shape: output shape of the neural net
+    :type output_shape: tuple
+    """
+    def __init__(self, img_shape: Tuple[int, int], output_shape: tuple):
+        super(SimpleConvNetModel, self).__init__()
+        # TODO: Set up Conv Net of your choosing. You can / should hardcode
+        # the sizes and layers of this Neural Net. The img_size tells you what
+        # the input size should be and you have to determine the best way to
+        # represent the output_shape (tuple of 2 ints, tuple of 1 int, just an
+        # int , etc).
+        raise NotImplementedError()
+    
+    def forward(x):
+        """forward generates the prediction for the input x.
+        
+        :param x: Input array of size (Batch,Input_layer_size)
+        :type x: np.ndarray
+        :return: The prediction of the model
+        :rtype: np.ndarray
+        """
+        raise NotImplementedError()
+
+
+if __name__ == "__main__":
+  ## You can use code similar to that used in the LinearRegression file to 
+  # load and train the model.
+  pass
